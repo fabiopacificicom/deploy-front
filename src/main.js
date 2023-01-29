742e5dbb8dc1a698d00c788c2b7ec8a1ee7a95a6
fbhood <mail@fabiopacifici.com>
2023-01-29 12:09:24 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+createApp(App).mount('#app')
